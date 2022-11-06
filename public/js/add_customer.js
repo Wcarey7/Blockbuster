fd0527881f6e8607386f2ab38abe5a66b4ff446e
Wade Carey <wadecarey7@gmail.com>
2022-11-06 15:27:39 -0800

    Update add_customer.js

--- public/js/add_customer.js
+++ public/js/add_customer.js
@@ -144,4 +144,13 @@ addRowToTable = (data) => {
 
     // Add the row to the table
     currentTable.appendChild(row);
+
+
+    // Find drop down menu, create a new option, fill data in the option (full name, id),
+    // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh
+    let selectMenu = document.getElementById("mySelect");
+    let option = document.createElement("option");
+    option.text = newRow.first_name + ' ' +  newRow.last_name;
+    option.value = newRow.customer_id;
+    selectMenu.add(option);
 }
