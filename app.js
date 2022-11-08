7bd7bb5bd92b2022dbc7f9eb7d4a746b25308fea
Wcarey7 <wadecarey7@gmail.com>
2022-11-07 22:35:14 -0800

    added Movies and route

--- app.js
+++ app.js
@@ -239,6 +239,56 @@ app.post('/add-order-ajax', function(req, res){
 });
 
 
+
+
+app.post('/add-movie-ajax', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let release_date = parseInt(data.release_date);
+    if (isNaN(release_date))
+    {
+        release_date = 'NULL'
+    }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Movies (movie_title, release_date, genre) VALUES ('${data.movie_title}', '${data.release_date}', '${data.genre}')`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = `SELECT * FROM Movies;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+
+
+
 app.delete('/delete-customer-ajax/', function(req,res,next){
     let data = req.body;
     let customerID = parseInt(data.id);
