0980ed26165bfe97d91b207ca6f525eea6cfc7a8
sabinand <sabinand@oregonstate.edu>
2022-12-03 22:48:44 -0800

    fixed the issues that were affecting delete, update, and search function to not work properly

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-//Referenced from: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+// Main Routes
+
+// Code Citations
+// Date: 11/15/2022
+// Routes Referenced from:
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app 
+
 
 /*
     SETUP
@@ -17,17 +23,14 @@ var db = require('./database/db-connector');
 
 // Handlebars
 const { engine } = require('express-handlebars');
-var exphbs = require('express-handlebars');     // Import express-handlebars
-
-
-app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
-app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+var exphbs = require('express-handlebars');
+app.engine('.hbs', engine({extname: ".hbs"}));
+app.set('view engine', '.hbs');
 
 
 /*
     ROUTES
 */
-
 
 /*////////////////////////////////////////////////////////////////////////////////////////
                 INDEX
@@ -40,16 +43,19 @@ app.get('/', function(req, res)
     // If there is no query string, perform SELECT
     if (req.query.lname === undefined)
     {
-        query1 = `SELECT customer_id AS ID, first_name AS "First Name", last_name AS "Last Name", customer_street AS Street, 
-        customer_city AS City, customer_state AS State, customer_zip AS Zip, customer_phone_number AS "Phone Number", 
-        customer_active_rentals AS "Active Rentals", customer_total_rentals AS "Total Rentals" FROM Customers;`
+        query1 = `SELECT customer_id AS ID, first_name AS "First Name", last_name AS "Last Name", 
+        customer_street AS Street, customer_city AS City, customer_state AS State, customer_zip AS Zip, 
+        customer_phone_number AS "Phone Number", customer_active_rentals AS "Active Rentals", 
+        customer_total_rentals AS "Total Rentals" 
+        FROM Customers;`
     }
 
     // If there is a query string, search
     else
     {
-        query1 = `SELECT customer_id AS ID, first_name AS "First Name", last_name AS "Last Name", customer_street AS Street, 
-        customer_city AS City, customer_state AS State, customer_zip AS Zip, customer_phone_number AS "Phone Number", 
+        query1 = `SELECT customer_id AS ID, first_name AS "First Name", last_name AS "Last Name", 
+        customer_street AS Street, customer_city AS City, customer_state AS State, 
+        customer_zip AS Zip, customer_phone_number AS "Phone Number", 
         customer_active_rentals AS "Active Rentals", customer_total_rentals AS "Total Rentals" 
         FROM Customers WHERE last_name LIKE "${req.query.lname}%"`
     }
@@ -64,7 +70,7 @@ app.get('/', function(req, res)
 
 
 
-//ADD
+// ADD
 app.post('/add-customer-ajax', function(req, res)
 {
     let data = req.body;
@@ -125,15 +131,16 @@ app.post('/add-customer-ajax', function(req, res)
 
 
 
-//UPDATE
+// UPDATE
 app.put('/put-customer-ajax', function(req,res,next)
 {
     let data = req.body;
     let customerID = parseInt(data.customerId);
 
-    let queryUpdateCustomer = `UPDATE Customers SET first_name = ?, last_name = ?, customer_street = ?, customer_city = ?,
-    customer_state = ?, customer_zip = ?, customer_phone_number = ?, customer_active_rentals = ?,
-    customer_total_rentals = ? WHERE customer_id = ?`;
+    let queryUpdateCustomer = `UPDATE Customers SET first_name = ?, last_name = ?, customer_street = ?, 
+    customer_city = ?, customer_state = ?, customer_zip = ?, customer_phone_number = ?, 
+    customer_active_rentals = ?, customer_total_rentals = ? 
+    WHERE customer_id = ?`;
 
     selectCustomer =  `SELECT * FROM Customers;`;
 
@@ -164,8 +171,7 @@ app.put('/put-customer-ajax', function(req,res,next)
 });
 
 
-
-//DELETE
+// DELETE
 app.delete('/delete-customer-ajax/', function(req,res,next)
 {
     let data = req.body;
@@ -186,7 +192,6 @@ app.delete('/delete-customer-ajax/', function(req,res,next)
 
 
 
-
 /*////////////////////////////////////////////////////////////////////////////////////////
                     ORDERS
 */
@@ -196,24 +201,12 @@ app.get('/orders', function(req, res)
     let customers = "SELECT * FROM Customers;";
     let locations = "SELECT * FROM Locations;";
 
-    // If there is no query string, perform SELECT
-    if (req.query.filter === undefined)
-    {
-        query1 = `SELECT order_id AS ID, customer_id AS Customer_Name, location_id AS Location_Address, 
-        DATE_FORMAT(order_date, "%m-%d-%Y") AS Order_Date, DATE_FORMAT(return_date, "%m-%d-%Y") AS Return_Date, over_due AS Is_Overdue 
-        FROM Orders;`
+    query1 = `SELECT order_id AS ID, customer_id AS Customer_Name, location_id AS Location_Address, 
+    DATE_FORMAT(order_date, "%m-%d-%Y") AS Order_Date, DATE_FORMAT(return_date, "%m-%d-%Y") AS Return_Date, 
+    IF(over_due=0, "No", "Yes") AS Is_Overdue 
+    FROM Orders;`
         
-    }
-    // If there is a query string, search
-    else
-    {
-        query1 = `SELECT order_id AS ID, customer_id AS Customer_Name, location_id AS Location_Address, 
-        DATE_FORMAT(order_date, "%m-%d-%Y") AS Order_Date, DATE_FORMAT(return_date, "%m-%d-%Y") AS Return_Date, over_due AS Is_Overdue 
-        FROM Orders WHERE location_id LIKE "${req.query.filter}%"`
-    }
-
     db.pool.query(query1, function(error, rows, fields){
-        
         let orders = rows;
         
         // Run the second query
@@ -226,6 +219,57 @@ app.get('/orders', function(req, res)
                 customermap[id] = customer["first_name"] + ' ' + customer["last_name"];
             });
 
+            // Run the third query
+            db.pool.query(locations, (error, rows, fields) => {
+                let locations = rows;
+                //Map to replace location_id with location address
+                let locationmap = {}
+                locations.map(location => {
+                    let id = parseInt(location.location_id, 10);
+                    locationmap[id] = location["location_street"] + ', ' + location["location_city"]
+                    + ', ' + location["location_state"] + ' ' + location["location_zip"];
+                });
+
+                orders = orders.map(order => {
+                    return Object.assign(order, {Customer_Name: customermap[order.Customer_Name], 
+                        Location_Address: locationmap[order.Location_Address],
+                    });
+                });
+
+                return res.render('orders', {
+                    data: orders, customers: customers, locations: locations,
+                });
+            });
+        });
+    });
+});
+
+
+// SEARCH / FILTER
+app.get('/orders/:filter', function(req, res)
+{
+    let query1;
+    let customers = "SELECT * FROM Customers;";
+    let locations = "SELECT * FROM Locations;";
+    let {filter} = req.params;   
+
+    query1 = `SELECT order_id AS ID, customer_id AS Customer_Name, location_id AS Location_Address, 
+    DATE_FORMAT(order_date, "%m-%d-%Y") AS Order_Date, DATE_FORMAT(return_date, "%m-%d-%Y") AS Return_Date, 
+    IF(over_due=0, "No", "Yes") AS Is_Overdue 
+    FROM Orders WHERE ?? LIKE "${req.query.filter}%"`
+
+    db.pool.query(query1, [filter], function(error, rows, fields){
+        let orders = rows;
+        
+        // Run the second query
+        db.pool.query(customers, (error, rows, fields) => {
+            let customers = rows;
+            //Map to replace customer_id with customer name
+            let customermap = {}
+            customers.map(customer => {
+                let id = parseInt(customer.customer_id, 10);
+                customermap[id] = customer["first_name"] + ' ' + customer["last_name"];
+            });
 
             // Run the third query
             db.pool.query(locations, (error, rows, fields) => {
@@ -254,8 +298,7 @@ app.get('/orders', function(req, res)
 
 
 
-
-//ADD
+// ADD
 app.post('/add-order-ajax', function(req, res)
 {
     let data = req.body;
@@ -272,7 +315,8 @@ app.post('/add-order-ajax', function(req, res)
 
     let query2 = `SELECT order_id, CONCAT(Customers.first_name, " ", Customers.last_name) AS Customer_Name,  
     CONCAT(Locations.location_street, ", ", Locations.location_city, ", ", Locations.location_state," ", Locations.location_zip) AS Location_Address, 
-    DATE_FORMAT(order_date, "%m-%d-%Y") AS Order_Date, DATE_FORMAT(return_date, "%m-%d-%Y") AS Return_Date, over_due AS Is_Overdue 
+    DATE_FORMAT(order_date, "%m-%d-%Y") AS Order_Date, DATE_FORMAT(return_date, "%m-%d-%Y") AS Return_Date, 
+    IF(over_due=0, "No", "Yes") AS Is_Overdue 
     FROM Orders
     LEFT JOIN Customers ON Orders.customer_id = Customers.customer_id 
     LEFT JOIN Locations ON Orders.location_id = Locations.location_id;`
@@ -303,9 +347,7 @@ app.post('/add-order-ajax', function(req, res)
 
 
 
-
-
-//UPDATE
+// UPDATE
 app.put('/put-order-ajax', function(req,res,next)
 {
     let data = req.body;
@@ -316,7 +358,8 @@ app.put('/put-order-ajax', function(req,res,next)
 
     let selectOrder = `SELECT order_id AS ID, CONCAT(Customers.first_name, " ", Customers.last_name) AS Customer_Name,  
     CONCAT(Locations.location_street, ", ", Locations.location_city, ", ", Locations.location_state," ", Locations.location_zip) AS Location_Address, 
-    DATE_FORMAT(order_date, "%m-%d-%Y") AS Order_Date, DATE_FORMAT(return_date, "%m-%d-%Y") AS Return_Date, over_due AS Is_Overdue 
+    DATE_FORMAT(order_date, "%m-%d-%Y") AS Order_Date, DATE_FORMAT(return_date, "%m-%d-%Y") AS Return_Date, 
+    IF(over_due=0, "No", "Yes") AS Is_Overdue 
     FROM Orders
     LEFT JOIN Customers ON Orders.customer_id = Customers.customer_id 
     LEFT JOIN Locations ON Orders.location_id = Locations.location_id;`  
@@ -346,8 +389,7 @@ app.put('/put-order-ajax', function(req,res,next)
 });
 
 
-
-//DELETE
+// DELETE
 app.delete('/delete-order-ajax/', function(req,res,next)
 {
     let data = req.body;
@@ -368,8 +410,6 @@ app.delete('/delete-order-ajax/', function(req,res,next)
 
 
 
-
-
 /*////////////////////////////////////////////////////////////////////////////////////////
                     ORDERED_MOVIES
 */
@@ -382,27 +422,26 @@ app.get('/ordered_movies', function(req, res)
     // If there is no query string, perform SELECT
     if (req.query.filter === undefined || req.query.filter === "")
     {
-        query1 = "SELECT ordered_movies_id AS ID, order_id AS Order_ID, movie_id AS Movie_Title, quantity AS Quantity FROM Ordered_Movies;";
+        query1 = `SELECT ordered_movies_id AS ID, order_id AS Order_ID, movie_id AS Movie_Title, quantity AS Quantity 
+        FROM Ordered_Movies;`;
     }
     // If there is a query string, search
     else
     {
         query1 = `SELECT ordered_movies_id AS ID, order_id AS Order_ID, movie_id AS Movie_Title, quantity AS Quantity 
-        FROM Ordered_Movies WHERE order_id = "${req.query.filter}%"`
+        FROM Ordered_Movies 
+        WHERE order_id = "${req.query.filter}%"`
     }
 
     db.pool.query(query1, function(error, rows, fields){
-        
         let orderedMovies = rows;
         
         // Run the second query
         db.pool.query(orders, (error, rows, fields) => {
-
             let orders = rows;
 
             // Run the third query
             db.pool.query(movies, (error, rows, fields) => {
-
                 let movies = rows;
                 //Map to replace movie_id with movie title
                 let moviemap = {}
@@ -426,7 +465,7 @@ app.get('/ordered_movies', function(req, res)
 
 
 
-//ADD
+// ADD
 app.post('/add-ordered-movie-ajax', function(req, res)
 {
     let data = req.body;
@@ -461,8 +500,7 @@ app.post('/add-ordered-movie-ajax', function(req, res)
 });
 
 
-
-//DELETE
+// DELETE
 app.delete('/delete-ordered-movies-ajax/', function(req,res,next)
 {
     let data = req.body;
@@ -480,8 +518,6 @@ app.delete('/delete-ordered-movies-ajax/', function(req,res,next)
             }
         })
 });
-
-
 
 
 
@@ -627,20 +663,18 @@ app.delete('/delete-movie-ajax/', function(req,res,next){
 
 
 
-
-
-
 /*////////////////////////////////////////////////////////////////////////////////////////
                     AVAILABLE_RENTALS
 */
 app.get('/available_rentals', function(req, res)
 {  
-    let query1 = "SELECT avail_id AS ID, movie_id AS MovieTitle, location_id AS Location, avail_copies AS 'Available Copies' FROM Available_Rentals;";
+    let query1 = `SELECT avail_id AS ID, movie_id AS MovieTitle, location_id AS Location, 
+    avail_copies AS 'Available Copies' FROM Available_Rentals;`;
+
     let movies = "SELECT * FROM Movies;";
     let locations = "SELECT * FROM Locations;";               
 
     db.pool.query(query1, function(error, rows, fields){
-        
         let availRentals = rows;
         
         // Run the second query
@@ -679,20 +713,24 @@ app.get('/available_rentals', function(req, res)
 });
 
 
-
-
-
-//ADD
+// ADD
 app.post('/add-available-rentals-ajax', function(req, res)
 {
     let data = req.body;
+
+    let locationId = parseInt(data.locationId);
+    if (isNaN(locationId))
+    {
+        locationId = null
+    }
     
     let query1 = `INSERT INTO Available_Rentals (movie_id, location_id, avail_copies)
-    VALUES ('${data.movieId}', '${data.locationId}', '${data.availCopies}')`;
+    VALUES ('${data.movieId}', ${locationId}, '${data.availCopies}')`;
 
     let query2 = `SELECT avail_id, movie_title, 
     CONCAT(Locations.location_street, ", ", Locations.location_city, ", ", Locations.location_state," ", Locations.location_zip) AS Location, avail_copies
-    FROM Available_Rentals LEFT JOIN Movies ON Available_Rentals.movie_id = Movies.movie_id 
+    FROM Available_Rentals 
+    LEFT JOIN Movies ON Available_Rentals.movie_id = Movies.movie_id 
     LEFT JOIN Locations ON Available_Rentals.location_id = Locations.location_id;`
 
     db.pool.query(query1, function(error, rows, fields){
@@ -719,7 +757,59 @@ app.post('/add-available-rentals-ajax', function(req, res)
 });
 
 
-//DELETE
+
+// UPDATE
+app.put('/put-available-rentals-ajax', function(req,res,next)
+{
+    let data = req.body;
+    let availID = parseInt(data.availId);
+    let movieId = parseInt(data.movieId);
+    let locationId = parseInt(data.locationId);
+    if (isNaN(movieId))
+    {
+        movieId = null
+    }
+    if (isNaN(locationId))
+    {
+        locationId = null
+    }
+
+    let queryUpdateAvailRental = `UPDATE Available_Rentals SET movie_id = ?, location_id = ?, avail_copies = ?
+    WHERE avail_id = ?`;
+
+    let selectAvailRental = `SELECT avail_id, movie_title, 
+    CONCAT(Locations.location_street, ", ", Locations.location_city, ", ", Locations.location_state," ", Locations.location_zip) AS Location, avail_copies
+    FROM Available_Rentals 
+    LEFT JOIN Movies ON Available_Rentals.movie_id = Movies.movie_id 
+    LEFT JOIN Locations ON Available_Rentals.location_id = Locations.location_id;`  
+    
+    db.pool.query(queryUpdateAvailRental,
+    [
+        movieId, locationId, data['availCopies'], availID,
+    ],
+    function(error, rows, fields){
+        if (error) {
+            console.log(error);
+            res.sendStatus(400);
+        }
+        // Update the front-end
+        else
+        {
+        db.pool.query(selectAvailRental, function(error, rows, fields) {
+            if (error) {
+                console.log(error);
+                res.sendStatus(400);
+            } else {
+                res.send(rows);
+            }
+        })
+        }
+  });
+});
+
+
+
+// DELETE
 app.delete('/delete-available-rentals-ajax/', function(req,res,next)
 {
     let data = req.body;
@@ -741,14 +831,25 @@ app.delete('/delete-available-rentals-ajax/', function(req,res,next)
 
 
 
-
 /*////////////////////////////////////////////////////////////////////////////////////////
                     LOCATIONS
 */
-//DISPLAY
 app.get('/locations', function(req, res)
 {  
-    let query1 = "SELECT * FROM Locations;";               
+    let query1 = "SELECT * FROM Locations;";
+    
+        // If there is no query string, we just perform a basic SELECT
+        if (req.query.location_state === undefined)
+        {
+            query1 = "SELECT * FROM Locations;";
+        }
+    
+        // If there is a query string, we assume this is a search, and return desired results
+        else
+        {
+            query1 = `SELECT * FROM Locations WHERE location_state LIKE "${req.query.location_state}%"`
+        }
+    
 
     db.pool.query(query1, function(error, rows, fields){    
 
@@ -762,21 +863,21 @@ app.post('/add-location-ajax', function(req, res)
     let data = req.body;
 
     // Capture NULL values
-    let zip = parseInt(data.location_zip);
-    if (isNaN(location_zip))
+    let zip = parseInt(data.zip);
+    if (isNaN(zip))
     {
         zip = 'NULL'
     }
 
-    let phone = parseInt(data.location_phone_number);
-    if (isNaN(location_phone_number))
+    let phone = parseInt(data.phone);
+    if (isNaN(phone))
     {
         phone = 'NULL'
     }
   
 
     query1 = `INSERT INTO Locations (location_street, location_city, location_state, location_zip, location_phone_number) 
-    VALUES ('${data.location_street}', '${data.location_city}', '${data.location_state}', '${data.location_zip}', '${data.location_phone_number}')`;
+    VALUES ('${data.street}', '${data.city}', '${data.state}', ${zip}, ${phone})`;
     db.pool.query(query1, function(error, rows, fields){
 
         if (error) {
@@ -808,11 +909,11 @@ app.put('/put-location-ajax', function(req,res,next)
     let data = req.body;
     let locationID = parseInt(data.locationId);
 
-    let queryUpdateCustomer = `UPDATE Location SET location_street = ?, location_city = ?, location_state = ?, location_phone_number = ?`;
+    let queryUpdateLocation = `UPDATE Locations SET location_street = ?, location_city = ?, location_state = ?, location_zip = ?, location_phone_number = ? WHERE location_id = ?`;
 
-    selectCustomer =  `SELECT * FROM Locations;`;
+    selectLocation =  `SELECT * FROM Locations;`;
 
-    db.pool.query(queryUpdateCustomer, 
+    db.pool.query(queryUpdateLocation, 
     [
         data['street'], data['city'], data['state'],
         data['zip'], data['phone'], locationID,
@@ -825,7 +926,7 @@ app.put('/put-location-ajax', function(req,res,next)
         // Update the front-end
         else
         {
-        db.pool.query(selectCustomer, function(error, rows, fields) {
+        db.pool.query(selectLocation, function(error, rows, fields) {
 
             if (error) {
                 console.log(error);
@@ -839,7 +940,7 @@ app.put('/put-location-ajax', function(req,res,next)
 });
 
 //DELETE
-app.delete('/delete-customer-ajax/', function(req,res,next)
+app.delete('/delete-location-ajax/', function(req,res,next)
 {
     let data = req.body;
     let locationID = parseInt(data.id);
@@ -856,6 +957,8 @@ app.delete('/delete-customer-ajax/', function(req,res,next)
             }
         });
 });
+
+
 
 /*
     LISTENER
